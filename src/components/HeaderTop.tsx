6c97c1d5bd14bf449c7e0d0d85c5970409241177
Luiz Felipe Gois <luizfelipegois.contact@gmail.com>
2024-03-18 13:52:13 -0300

    feat: Renders header at the top of the page

---
+++ src/components/HeaderTop.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const HeaderTop = () => {
+  return (
+    <header className="border-b border-gray-200 hidden sm:block">
+      <div className="container py-4">
+        <div className="flex justify-between items-center">
+          <div className="hidden lg:flex gap-1">
+            <button className="text-[14px] py-1 px-2 hover:text-gray-600">ENTRAR</button>
+            <button className="text-[14px] py-1 px-2 hover:text-gray-600">CADASTRAR</button>
+          </div>
+          <div className="text-gray-800 text-[14px]">
+            <b>FRETE GRÁTIS</b> ESSA SEMANA PEDIDO ACABOU - R$ 250,00
+          </div>
+          <div className="flex gap-4">
+            <select name="currency" id="currency" className="text-gray-800 text-[14px] cursor-pointer hover:text-gray-600">
+              <option value="BRL R$">BRL R$</option>
+              <option value="EUR €">EUR €</option>
+              <option value="USD $">USD $</option>
+            </select>
+            <select name="language" id="language" className="text-gray-800 text-[14px] cursor-pointer hover:text-gray-600">
+              <option value="Português">Português</option>
+              <option value="Inglês">Inglês</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default HeaderTop;
